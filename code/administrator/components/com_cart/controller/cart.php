a588d56b771de0f977fd47f398324f4221400278
Jebb Domingo <jebb@timble.net>
2016-11-20 23:12:42 +0800

    Refactoring

--- code/administrator/components/com_cart/controller/cart.php
+++ code/administrator/components/com_cart/controller/cart.php
@@ -17,6 +17,20 @@
  */
 abstract class ComCartControllerCart extends ComKoowaControllerModel
 {
+    /**
+     * Constructor.
+     *
+     * @param KObjectConfig $config Configuration options.
+     */
+    public function __construct(KObjectConfig $config)
+    {
+        @ini_set('max_execution_time', 300);
+        
+        parent::__construct($config);
+
+        $this->addCommandCallback('before.add', '_validateAdd');
+    }
+
     protected function _initialize(KObjectConfig $config)
     {
         $config->append(array(
@@ -29,6 +43,32 @@ abstract class ComCartControllerCart extends ComKoowaControllerModel
         $this->getObject('manager')->registerAlias('com:cart.controller.permission.cart', $permission);
 
         parent::_initialize($config);
+    }
+
+    protected function _validateAdd(KControllerContextInterface $context)
+    {
+        $data       = $context->request->data;
+        $translator = $this->getObject('translator');
+        $result     = false;
+
+        try
+        {
+            $cart     = $this->getModel()->fetch();
+            $quantity = (int) $data->quantity;
+
+            if (empty($data->row) || !$quantity) {
+                throw new KControllerExceptionRequestInvalid($translator->translate('Please select an item and specify its quantity'));
+            }
+
+            $result = true;
+        }
+        catch(Exception $e)
+        {
+            $context->response->setRedirect($context->request->getReferrer(), $e->getMessage(), 'error');
+            $context->response->send();
+        }
+
+        return $result;
     }
 
     protected function _actionRender(KControllerContextInterface $context)
@@ -50,5 +90,87 @@ abstract class ComCartControllerCart extends ComKoowaControllerModel
         }
 
         return parent::_actionRender($context);
+    }
+
+    protected function _actionAdd(KControllerContextInterface $context)
+    {
+        $data      = $context->request->data;
+        $cart      = $this->getModel()->fetch();
+        $cartItems = array();
+
+        if (count($cart))
+        {
+            // Add item(s) to the cart
+            if ($items = $cart->getItems())
+            {
+                foreach ($items as $item)
+                {
+                    $cartItems[] = $item->row;
+
+                    // Existing item, update quantity instead
+                    if ($item->row == $data->ItemRef)
+                    {
+                        $item->quantity += $data->quantity;
+                        $item->save();
+                    }
+                }
+            }
+
+            if (!in_array($data->ItemRef, $cartItems))
+            {
+                // New item
+                $cartItemData = array(
+                    'cart_id'  => $cart->id,
+                    'row'      => $data->row,
+                    'quantity' => $data->quantity,
+                );
+
+                $item = $this->getObject('com:cart.model.items')->create($cartItemData);
+                $item->save();
+            }
+        }
+
+        return $cart;
+    }
+
+    protected function _actionUpdatecart(KControllerContextInterface $context)
+    {
+        if (!$context->result instanceof KModelEntityInterface) {
+            $cart = $this->getModel()->fetch();
+        } else {
+            $cart = $context->result;
+        }
+
+        if (count($cart))
+        {
+            $cart->setProperties($context->request->data->toArray());
+            $cart->save();
+
+            if (in_array($cart->getStatus(), array(KDatabase::STATUS_FETCHED, KDatabase::STATUS_UPDATED)))
+            {
+                foreach ($cart->getItems() as $item)
+                {
+                    $item->quantity = (int) $context->request->data->quantity[$item->id];
+                    $item->save();
+                }
+
+                $context->response->addMessage('You shopping cart has been updated');
+            }
+            else $context->response->addMessage($cart->getStatusMessage(), 'error');
+        }
+    }
+
+    protected function _actionDeleteitem(KControllerContextInterface $context)
+    {
+        $data  = $context->request->data;
+        $ids   = $data->id;
+        $items = $this->getObject('com:cart.model.items')->id($ids)->fetch();
+
+        foreach ($items as $item) {
+            $item->delete();
+        }
+
+        $response = $context->getResponse();
+        $response->addMessage('Item has been deleted from your shopping cart');
     }
 }
